645e06d55c10756e7581a52d95429b2b5c65ae57
Brian <brianrlam@gmail.com>
2023-04-04 16:37:48 -0400

    finish tabulation no memo review

--- tabulation_project/lib/problems.js
+++ tabulation_project/lib/problems.js
@@ -21,6 +21,18 @@
 // stepper([3, 4, 1, 0, 10]);           // => true, because we can step through elements 3 -> 4 -> 10
 // stepper([2, 3, 1, 1, 0, 4, 7, 8])    // => false, there is no way to step to the end
 function stepper(nums) {
+    // TAB
+    const possible = new Array(nums.length).fill(false);
+    possible[0] = true;
+    for(let i=0;i<possible.length;i++){
+        if(!possible[i]) continue;
+        for(let j=1;j<=nums[i] && i+j<possible.length;j++){
+            possible[i+j]=true;
+        }
+    }
+    return possible[possible.length-1]
+
+    // MEM
 
 }
 
@@ -36,7 +48,17 @@ function stepper(nums) {
 // maxNonAdjacentSum([2, 7, 9, 3, 4])   // => 15, because 2 + 9 + 4
 // maxNonAdjacentSum([4,2,1,6])         // => 10, because 4 + 6 
 function maxNonAdjacentSum(nums) {
+    if(!nums.length) return 0;
+    // nums = [0,...nums]
+    const dp = new Array(nums.length);
+    dp[0] = nums[0];
+    dp[1] = Math.max(nums[0],nums[1]);
 
+    for(let i=2;i<nums.length;i++){
+        dp[i] = Math.max(dp[i-1],dp[i-2]+nums[i]);
+    }
+
+    return dp.at(-1);
 }
 
 
@@ -53,7 +75,16 @@ function maxNonAdjacentSum(nums) {
 // minChange([1, 5, 10, 25], 15)    // => 2, because 10 + 5 = 15
 // minChange([1, 5, 10, 25], 100)   // => 4, because 25 + 25 + 25 + 25 = 100
 function minChange(coins, amount) {
+    let dp = new Array(amount+1).fill(Infinity);
+    dp[0] = 0;
 
+    for(let i=0;i<dp.length;i++){
+        for(let coin of coins){
+            if(i+coin<=amount) dp[i+coin] = Math.min(dp[i+coin],dp[i]+1);
+        }
+    }
+
+    return dp.at(-1)
 }
 
 
